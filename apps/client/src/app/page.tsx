61e9058f63abb7cb890d1293d51900aebb441d96
Benito <benitopedro21@outlook.com>
2024-04-18 03:07:30 -0300

    🐛 FIX:

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { trpc } from "@client/trpc.ts";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const { greeting } = await trpc.hello.query({ name: `Tom` });
+  const { greeting } = await trpc.hello.query({ name: `Benito` });
   return (
     <>
       <div>{greeting}</div>
