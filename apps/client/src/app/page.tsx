8c28507834f6788044b9f5d477a103216ba3d5c3
Benito <benitopedro21@outlook.com>
2024-04-18 02:42:52 -0300

    🐛 FIX: Build Client error

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,12 +1,12 @@
-import { trpc } from "@client/app/trpc";
-import ClientSide from "@client/components/ClientSide";
+import { trpc } from "@client/trpc.ts";
+
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const { greeting } = await trpc.hello.query({ name: `Tom` });
   return (
     <>
       <div>{greeting}</div>
-      <ClientSide />
     </>
   );
 }
