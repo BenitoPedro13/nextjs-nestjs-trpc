e87ff18dc6d26da3bc6042050d27e0643cb1b508
Benito <benitopedro21@outlook.com>
2024-04-18 10:57:02 -0300

    👌 IMPROVE: removed unused imports

--- apps/client/src/actions.ts
+++ apps/client/src/actions.ts
@@ -1,10 +1,8 @@
 "use server";
 
-import { Dispatch, SetStateAction } from "react";
 import { trpc } from "./trpc";
 
 export async function create() {
-  "use server";
   const { greeting } = await trpc.hello.query({
     name: `Benito Client <= Server Action `,
   });
